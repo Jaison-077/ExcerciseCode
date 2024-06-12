55b84b60fedd18fac7e117d901c031f28ab038a5
Jaison <jaisonksebastian1999@gmail.com>
2024-06-12 11:35:06 +0530

    Added new class Multiply3Num Added Multiply3Num object.

---
+++ Multiply3Num.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseCode
+{
+    internal class Multiply3Num
+    {
+        public void printProgram()
+        {
+            int a, b, c;
+            Console.Write("Enter the First Num: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Second Num: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the Thrid Num: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Multiply of three: {a*b*c}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using ExcerciseCode;
 //HelloWorld obj = new HelloWorld();
 //SumOfTwo obj = new SumOfTwo();  
 //DivideTwo obj = new DivideTwo();
-SwapTwo obj = new SwapTwo();
+//SwapTwo obj = new SwapTwo();
+Multiply3Num obj = new Multiply3Num();
 
 obj.printProgram();
